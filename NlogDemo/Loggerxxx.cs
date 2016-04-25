fb6b3ee8191105ab6d32ae92c6ed43614cfe858d
helang <447111964@qq.com>
2016-04-25 21:40:18 +0800

    nlogDemo特性可以异步，性能很强大

---
+++ NlogDemo/Loggerxxx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlogDemo
+{
+    public class Loggerxxx
+    {
+        NLog.Logger logger;
+
+        private Loggerxxx(NLog.Logger logger)
+        {
+            this.logger = logger;
+        }
+
+        public Loggerxxx(string name)
+            : this(NLog.LogManager.GetLogger(name))
+        {
+        }
+
+        public static Loggerxxx Default { get; private set; }
+        static Loggerxxx()
+        {
+            Default = new Loggerxxx(NLog.LogManager.GetCurrentClassLogger());
+        }
+
+        public void Debug(string msg, params object[] args)
+        {
+            logger.Debug(msg, args);
+        }
+
+        public void Debug(string msg, Exception err)
+        {
+            logger.Debug(msg, err);
+        }
+
+        public void Info(string msg, params object[] args)
+        {
+            logger.Info(msg, args);
+        }
+
+        public void Info(string msg, Exception err)
+        {
+            logger.Info(msg, err);
+        }
+
+        public void Trace(string msg, params object[] args)
+        {
+            logger.Trace(msg, args);
+        }
+
+        public void Trace(string msg, Exception err)
+        {
+            logger.Trace(msg, err);
+        }
+
+        public void Error(string msg, params object[] args)
+        {
+            logger.Error(msg, args);
+        }
+
+        public void Error(string msg, Exception err)
+        {
+            logger.Error(msg, err);
+        }
+
+        public void Fatal(string msg, params object[] args)
+        {
+            logger.Fatal(msg, args);
+        }
+
+        public void Fatal(string msg, Exception err)
+        {
+            logger.Fatal(msg, err);
+        }
+    }
+}
